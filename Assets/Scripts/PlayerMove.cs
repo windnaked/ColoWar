e20d3b42fab8de8ca7a26263f96329a217269a06
windnaked <andrecardeira03@gmail.com>
2016-07-03 15:11:53 +0100

    Movimento da nave

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed = 2.0f;
+    Vector2 position;
+
+    void Start()
+    {
+        Vector2 position = new Vector2(this.transform.position.x, this.transform.position.y);
+    }
+
+    void Update()
+    {
+        var moveVec = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical"), 0) * moveSpeed;
+        transform.position += moveVec * moveSpeed * Time.deltaTime;
+    }
+
+
+}
